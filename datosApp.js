11b4709de529f70c6cdd125d572533a3ef1afd7f
Santiago Álvarez Osuca <82sao82@gmail.com>
2021-08-05 18:02:36 +0200

    API FichajesUser

--- datosApp.js
+++ datosApp.js
@@ -2,6 +2,24 @@ const conexion = require('./conexion');
 
 
 module.exports = () => {
+    crearUserCognito = async(nombre, email, sub) => {
+        let sql = "SELECT DISTINCT name FROM sys.databases WHERE name LIKE 'Fac%' AND name NOT LIKE '%_bak'AND name NOT IN ('Fac_Demo', 'Fac_Prueba')";
+        let empresas = await conexion.recHit('Hit', sql);
+        let empresa = undefined, idTrabajador = undefined;
+        for(let indexEmpresa in empresas.recordset) {
+            let {name} = empresas.recordset[indexEmpresa];
+            let usersMailsAndIds = await conexion.recHit(name, "SELECT id, valor FROM dependentesextes WHERE nom = 'EMAIL' AND valor NOT LIKE ''");
+            for(let indexCorreo in usersMailsAndIds.recordset) {
+                if(email === usersMailsAndIds.recordset[indexCorreo].valor) {
+                    empresa = name;
+                    idTrabajador = usersMailsAndIds.recordset[indexCorreo].id;
+                    break;
+                }
+            }
+            if(empresa !== undefined) break;
+        }
+        await conexion.recHit('Hit', `INSERT INTO cognitoUsersSUB (idTrabajador, nombre, mail, empresa, SUB) VALUES (${idTrabajador}, '${nombre}', '${email}', '${empresa}', '${sub}')`);
+    }
     listarUsuarios = async (empresa) => {
         let usuarios = await conexion.recHit(`Fac_${empresa}`, 'SELECT * FROM Dependentes');
         let dataUsuarios = [];
@@ -19,5 +37,19 @@ module.exports = () => {
     listarUser = async (empresa, nombre) => {
         let user = await conexion.recHit(`Fac_${empresa}`, `SELECT CODI FROM Dependentes WHERE NOM like '${nombre}%'`);
         console.log(user);
+    }
+    fichajesUser = async (sub) => {
+        let userInfo = await conexion.recHit('Hit', `SELECT idTrabajador, empresa FROM cognitoUsersSUB WHERE sub = '${sub}'`);
+        let {idTrabajador, empresa} = userInfo.recordset[0];
+        let sql = `
+            SELECT tmst as fichaje FROM (SELECT TOP 1 * FROM cdpDadesFichador WHERE usuari = ${idTrabajador} AND accio = 1 ORDER BY tmst DESC) AS a 
+            UNION ALL
+            SELECT tmst as fichaje FROM(SELECT TOP 1 * FROM cdpDadesFichador WHERE usuari = ${idTrabajador} AND accio = 2 ORDER BY tmst DESC) AS b
+        `;
+        let ultimosFichajes = await conexion.recHit(empresa, sql);
+        return {
+            ultimoFichajeEntrada: ultimosFichajes.recordset[0].fichaje,
+            ultimoFichajeSalida: ultimosFichajes.recordset[1].fichaje
+        }
     }
 }
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 require('./datosApp.js')();
 const app = express();
-const port = 3000;
+const port = 3030;
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cors());
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
@@ -21,6 +23,17 @@ app.get('/users', (req, res) => {
 })
 app.get('/user', (req, res) => {
     listarUser(req.query.empresa, req.query.nombre).then((data) => {
+        res.send(data);
+    })
+})
+app.post('/cognitoNewUser', (req, res) => {
+    let {nombre, email, sub} = req.body;
+    crearUserCognito(nombre, email, sub).then(() => {
+        res.send('Usuario creado');
+    })
+})
+app.get('/fichajesUser', (req, res) => {
+    fichajesUser(req.query.sub).then((data) => {
         res.send(data);
     })
 })
